f0711d3b22c4c1dbdc1ca5868c3ff377288479c6
RafaelCarvalho <carvalhoferreirarafael@gmail.com>
2026-02-14 12:57:45 -0300

    [API] Acesso ao painel adminstrativo por /api/admin/

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     # Prefixo
     path("api/v1/", include("v1.urls")),
 ]
