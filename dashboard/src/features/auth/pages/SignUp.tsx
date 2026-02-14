62823e7aef7fe6b389c1bf3ee2b63e68d22fecdd
RafaelCarvalho <carvalhoferreirarafael@gmail.com>
2026-02-14 10:50:12 -0300

    Voltando ao antigo....

--- dashboard/src/features/auth/pages/SignUp.tsx
+++ dashboard/src/features/auth/pages/SignUp.tsx
@@ -51,13 +51,6 @@ export default function SignUpPage() {
           return;
         }
 
-<<<<<<< HEAD
-=======
-        const companyResponse = await companiesService.getById(
-          inviteData.company_id,
-        );
-
->>>>>>> parent of b512fdf ([UI] Correção página sign-up)
         setInvite(inviteData);
         setCompany(inviteData.company);
       } catch (err) {
