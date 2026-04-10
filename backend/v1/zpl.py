0078bda042963b117729f2563f2a6044463bf0c0
RafaelCarvalho <carvalhoferreirarafael@gmail.com>
2026-04-10 00:41:22 -0300

    [ZPL] Ajuste ZPL pusleira (centralizado e invertido)

--- backend/v1/zpl.py
+++ backend/v1/zpl.py
@@ -11,11 +11,12 @@ def generate_zpl_label(name: str, company: str = "Staff", qr_data: str = "") -> 
 
     zpl = (
         "^XA"
-        #"^PW640"           # Largura (80mm)
-        #"^LL200"           # Altura (25mm)
-        f"^FO120,600^A0R,40,40^FD{display_name}^FS"
-        f"^FO80,600^A0R,30,30^FD{company}^FS"
-        f"^FO10,400^BQN,2,6^FDQA,{qr_data}^FS"
+        "^POI" # Inverte a impressão para começar na ponta de saída
+        "^PW200"           # Largura (25mm)
+        "^LL2344"           # Altura (289mm)
+        f"^FO120,640^A0R,40,40^FD{display_name}^FS"
+        f"^FO80,640^A0R,30,30^FD{company}^FS"
+        f"^FO10,440^BQN,2,6^FDQA,{qr_data}^FS"
         "^XZ"
     )
 
