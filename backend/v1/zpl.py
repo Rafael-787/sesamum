75521b0bbb5f20f33a9131bb2181c3036eec7391
RafaelCarvalho <carvalhoferreirarafael@gmail.com>
2026-04-18 11:11:44 -0300

    [zpl] remoção de acentos

--- backend/v1/zpl.py
+++ backend/v1/zpl.py
@@ -1,3 +1,10 @@
+import unicodedata
+
+def normalizar(texto):
+    normalized = unicodedata.normalize('NFD', texto)
+    
+    return "".join(c for c in normalized if unicodedata.category(c) != 'Mn') 
+
 def generate_zpl_label(name: str, company: str = "Staff", qr_data: str = "", event:str = "") -> str:
     """
     Gera os comandos ZPL para impressão da pulseira/etiqueta.
@@ -7,18 +14,17 @@ def generate_zpl_label(name: str, company: str = "Staff", qr_data: str = "", eve
     ^BQN = QR Code
     """
     # Limita o tamanho do nome se for muito grande para não cortar a pulseira
-    display_name = name[:25] 
+    display_name = name[:25]
 
     zpl = (
         "^XA"
-        "^CI28" # UTF8
         "^POI" # Inverte a impressão para começar na ponta de saída
-        "^PW200"           # Largura (25mm)
+        "^PW200"           # Largura (25mm)''
         "^LL2344"           # Altura (289mm)
-        f"^FO120,640^A0R,40,40^FD{display_name}^FS"
-        f"^FO80,640^A0R,30,30^FD{company}^FS"
-        f"^FO40,640^A0R,30,30^FD{event}^FS"
-        f"^FO10,440^BQN,2,6^FDQA,{qr_data}^FS"
+        f"^FO120,640^A0R,40,40^FD{normalizar(display_name)}^FS"
+        f"^FO80,640^A0R,30,30^FD{normalizar(company)}^FS"
+        f"^FO40,640^A0R,30,30^FD{normalizar(event)}^FS"
+        f"^FO10,440^BQN,2,6^FDQA,{normalizar(qr_data)}^FS"
         "^XZ"
     )
 
